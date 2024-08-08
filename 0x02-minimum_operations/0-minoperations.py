2558a225f0c5b1b55bb9c3a533556671d2256fc0
Keitumetse K <keitumetselovely@gmail.com>
2024-08-08 11:43:41 +0000

    fixes

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
 def minOperations(n):
+    """
+    Calculates the minimum number of operations required
+    to achieve exactly n 'H' characters in a file.
+
+    Operations available:
+    1. Copy All
+    2. Paste
+
+    Args:
+    n (int): The target number of 'H' characters.
+
+    Returns:
+    int: The minimum number of operations needed. If n is less than 1, returns 0.
+    """
     if n <= 1:
         return 0
     
