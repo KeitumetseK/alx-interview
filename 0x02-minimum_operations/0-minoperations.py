d40a56505ccfc0d8785aec24262a5a89e1644918
Keitumetse K <keitumetselovely@gmail.com>
2024-08-08 11:39:25 +0000

    fixes

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,15 +2,20 @@
 def minOperations(n):
     if n <= 1:
         return 0
-
+    
     operations = 0
-    factor = 2
+    current = 1
+    clipboard = 0
 
-    while n > 1:
-        while n % factor == 0:
-            operations += factor
-            n //= factor
-        factor += 1
-
+    while current < n:
+        remaining = n - current
+        
+        if remaining % current == 0:
+            clipboard = current  # Copy All
+            operations += 1
+        
+        current += clipboard  # Paste
+        operations += 1
+    
     return operations
 
