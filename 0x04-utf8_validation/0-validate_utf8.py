e81a6616afd2e39d7a2266f3d8d744b906f4c342
Keitumetse K <keitumetselovely@gmail.com>
2024-08-26 09:48:16 +0000

    fixes

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,30 +1,46 @@
 #!/usr/bin/python3
+"""
+UTF-8 Validation Module
+"""
 
 def validUTF8(data):
-    # Number of bytes to process in the current UTF-8 character
+    """
+    Method to determine if a given data set represents a valid UTF-8 encoding.
+
+    :param data: List of integers representing bytes.
+    :return: True if the data is a valid UTF-8 encoding, else False.
+    """
     num_bytes = 0
-    
+
+    # Masks to check the leading bits of each byte
+    mask1 = 1 << 7  # 10000000
+    mask2 = 1 << 6  # 01000000
+
     for num in data:
-        # Ensure that the byte is within the valid range (0-255)
+        # Check if the current integer fits in a byte
         if num > 255:
             return False
         
+        mask = 1 << 7
         if num_bytes == 0:
             # Determine the number of bytes in the UTF-8 character
-            if (num >> 5) == 0b110:       # 2-byte character
-                num_bytes = 1
-            elif (num >> 4) == 0b1110:    # 3-byte character
-                num_bytes = 2
-            elif (num >> 3) == 0b11110:   # 4-byte character
-                num_bytes = 3
-            elif (num >> 7) != 0:         # 1-byte character must start with 0
+            while mask & num:
+                num_bytes += 1
+                mask = mask >> 1
+            
+            # 1-byte character or invalid byte length
+            if num_bytes == 0:
+                continue
+            if num_bytes == 1 or num_bytes > 4:
                 return False
         else:
-            # Check that the next byte starts with '10'
-            if (num >> 6) != 0b10:
+            # Check if it is a continuation byte ('10xxxxxx')
+            if not (num & mask1 and not (num & mask2)):
                 return False
-            num_bytes -= 1
+        
+        # Decrement the number of bytes left to check
+        num_bytes -= 1
 
-    # If there are no remaining expected continuation bytes, return True
+    # Return true if all bytes were correctly processed
     return num_bytes == 0
 
