491d1fb5afd82e39444cfb98c73229fe498ac3e2
Keitumetse K <keitumetselovely@gmail.com>
2024-08-26 09:42:09 +0000

    task 0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,40 +1,30 @@
 #!/usr/bin/python3
 
 def validUTF8(data):
-    # Number of bytes in the current UTF-8 character
+    # Number of bytes to process in the current UTF-8 character
     num_bytes = 0
     
-    # Masks to check the most significant bits
-    mask1 = 1 << 7  # 10000000
-    mask2 = 1 << 6  # 01000000
-
     for num in data:
         # Ensure that the byte is within the valid range (0-255)
         if num > 255:
             return False
         
-        mask = 1 << 7
         if num_bytes == 0:
             # Determine the number of bytes in the UTF-8 character
-            while mask & num:
-                num_bytes += 1
-                mask = mask >> 1
-            
-            # 1-byte character
-            if num_bytes == 0:
-                continue
-            
-            # UTF-8 can be 1 to 4 bytes long
-            if num_bytes == 1 or num_bytes > 4:
+            if (num >> 5) == 0b110:       # 2-byte character
+                num_bytes = 1
+            elif (num >> 4) == 0b1110:    # 3-byte character
+                num_bytes = 2
+            elif (num >> 3) == 0b11110:   # 4-byte character
+                num_bytes = 3
+            elif (num >> 7) != 0:         # 1-byte character must start with 0
                 return False
         else:
             # Check that the next byte starts with '10'
-            if not (num & mask1 and not (num & mask2)):
+            if (num >> 6) != 0b10:
                 return False
-        
-        # Decrease the number of bytes to process
-        num_bytes -= 1
-    
-    # All characters must be processed
+            num_bytes -= 1
+
+    # If there are no remaining expected continuation bytes, return True
     return num_bytes == 0
 
