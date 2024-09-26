8bd27c7486d554174831cd0302ea910608c679bb
KeitumetseK <keitumetselovely@gmail.com>
2024-09-26 15:14:58 +0000

    update

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
-total = 37
-list_of_coins1 = [1,2,25]
-
 def makeChange(coins, total):
     """
     Determines the fewest number of coins needed to meet a given total.
@@ -14,29 +11,17 @@ def makeChange(coins, total):
     Returns:
         int: Fewest number of coins needed, or -1 if it's not possible.
     """
-    if total <= 0:
-        return 0
-
-    remainder = total 
-
-    coins_needed = 0
-
-    coins_index = 0
-
-    sorted_coins_list = sorted(coins, reverse=True)
-
-    list_len = len(coins)
-
-    while remainder > 0:
-
-        if coins_index >- list_len:
-            return -1
-
-        if remainder - sorted_coins_list[coin_index] >- 0:
-            remainder -= sorted_coins_list[coin_index]
-
-            coins_needed += 1
-        else:
-            coins_index += 1
-    return coins_needed
-
+    def makeChange(coins, total):
+        if total <= 0:
+            return 0
+    
+    # Create a DP array to store the minimum number of coins for each amount
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+    
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+    
+    # If the value at dp[total] is infinity, it means it's not possible to form that total
+    return dp[total] if dp[total] != float('inf') else -1
