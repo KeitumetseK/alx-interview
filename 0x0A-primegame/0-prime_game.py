74039989b40c82a069ed9c335f71105e2f78f7f0
KeitumetseK <keitumetselovely@gmail.com>
2024-10-06 17:37:34 +0000

    primegame

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,49 +1,38 @@
 #!/usr/bin/python3
 
-def sieve_of_eratosthenes(n):
-    """ Implements the Sieve of Eratosthenes to find all primes up to n """
-    prime = [True] * (n + 1)
-    prime[0], prime[1] = False, False  # 0 and 1 are not prime numbers
-    p = 2
-    while p * p <= n:
-        if prime[p]:
-            for i in range(p * p, n + 1, p):
-                prime[i] = False
-        p += 1
-    return prime
-
-def count_primes_up_to_n(prime, n):
-    """ Count the number of primes less than or equal to n """
-    return sum(1 for i in range(2, n + 1) if prime[i])
-
 def isWinner(x, nums):
-    """ Determines the winner of the prime game """
-    if x < 1 or not nums:
-        return None
+    # Initialize a list to keep track of whether numbers are prime
+    max_n = max(nums)
+    is_prime = [True] * (max_n + 1)
+    is_prime[0] = is_prime[1] = False  # 0 and 1 are not prime numbers
 
-    max_n = max(nums)  # Find the maximum n to optimize sieve range
-    prime = sieve_of_eratosthenes(max_n)  # Generate primes up to the maximum n
-    
-    maria_wins = 0
-    ben_wins = 0
+    # Sieve of Eratosthenes to find all primes up to max_n
+    for p in range(2, int(max_n**0.5) + 1):
+        if is_prime[p]:
+            for multiple in range(p * p, max_n + 1, p):
+                is_prime[multiple] = False
+
+    # Dictionary to hold the count of wins for each player
+    wins = {"Maria": 0, "Ben": 0}
 
     for n in nums:
-        prime_count = count_primes_up_to_n(prime, n)
-        if prime_count % 2 == 1:
-            maria_wins += 1  # Maria wins if the count of primes is odd
-        else:
-            ben_wins += 1  # Ben wins if the count of primes is even
+        if n < 2:
+            wins["Ben"] += 1  # If n < 2, Maria can't pick any prime; Ben wins
+            continue
 
-    if maria_wins > ben_wins:
+        # Count the number of primes less than or equal to n
+        prime_count = sum(1 for i in range(n + 1) if is_prime[i])
+
+        # If the number of primes is odd, Maria wins; if even, Ben wins
+        if prime_count % 2 == 1:
+            wins["Maria"] += 1
+        else:
+            wins["Ben"] += 1
+
+    # Determine the overall winner
+    if wins["Maria"] > wins["Ben"]:
         return "Maria"
-    elif ben_wins > maria_wins:
+    elif wins["Ben"] > wins["Maria"]:
         return "Ben"
     else:
         return None
-
-# Example usage:
-if __name__ == "__main__":
-    x = 5
-    nums = [2, 5, 1, 4, 3]
-    print("Winner: {}".format(isWinner(x, nums)))
-
