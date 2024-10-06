15e282f219509a22831ab7389f4e9563f2a30735
KeitumetseK <keitumetselovely@gmail.com>
2024-10-06 17:24:37 +0000

    primegame

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,51 +1,49 @@
 #!/usr/bin/python3
+
+def sieve_of_eratosthenes(n):
+    """ Implements the Sieve of Eratosthenes to find all primes up to n """
+    prime = [True] * (n + 1)
+    prime[0], prime[1] = False, False  # 0 and 1 are not prime numbers
+    p = 2
+    while p * p <= n:
+        if prime[p]:
+            for i in range(p * p, n + 1, p):
+                prime[i] = False
+        p += 1
+    return prime
+
+def count_primes_up_to_n(prime, n):
+    """ Count the number of primes less than or equal to n """
+    return sum(1 for i in range(2, n + 1) if prime[i])
+
 def isWinner(x, nums):
-    """
-    Determines the winner of the game after x rounds.
-
-    Args:
-        x (int): Number of rounds.
-        nums (List[int]): List of numbers for each round.
-
-    Returns:
-        str or None: The name of the player with the most wins, "Maria" or "Ben".
-                     Returns None if the winner cannot be determined.
-    """
+    """ Determines the winner of the prime game """
     if x < 1 or not nums:
         return None
 
-    # Find the maximum number in the rounds
-    max_num = max(nums)
-
-    # Create a list to mark primes using the Sieve of Eratosthenes
-    sieve = [True] * (max_num + 1)
-    sieve[0] = sieve[1] = False  # 0 and 1 are not prime numbers
-
-    for i in range(2, int(max_num**0.5) + 1):
-        if sieve[i]:
-            for j in range(i * i, max_num + 1, i):
-                sieve[j] = False
-
-    # Precompute the number of primes less than or equal to any given n
-    prime_count = [0] * (max_num + 1)
-    for i in range(1, max_num + 1):
-        prime_count[i] = prime_count[i - 1] + (1 if sieve[i] else 0)
-
-    # Maria wins if the number of primes in the round is odd, Ben wins if it's even
+    max_n = max(nums)  # Find the maximum n to optimize sieve range
+    prime = sieve_of_eratosthenes(max_n)  # Generate primes up to the maximum n
+    
     maria_wins = 0
     ben_wins = 0
 
     for n in nums:
-        if prime_count[n] % 2 == 1:
-            maria_wins += 1
+        prime_count = count_primes_up_to_n(prime, n)
+        if prime_count % 2 == 1:
+            maria_wins += 1  # Maria wins if the count of primes is odd
         else:
-            ben_wins += 1
+            ben_wins += 1  # Ben wins if the count of primes is even
 
-    # Determine the overall winner
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
         return "Ben"
     else:
         return None
+
+# Example usage:
+if __name__ == "__main__":
+    x = 5
+    nums = [2, 5, 1, 4, 3]
+    print("Winner: {}".format(isWinner(x, nums)))
 
